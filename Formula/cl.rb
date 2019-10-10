c8e04561e0a9a2bed78fbdaafd8b950254b0cdd6
Marc Addeo <marcaddeo@gmail.com>
2019-10-10 16:13:30 -0400

    Bump cl to 0.4.0

--- Formula/cl.rb
+++ Formula/cl.rb
@@ -1,8 +1,8 @@
 class Cl < Formula
   desc "Command-line tool for managing a Keep A Changelog format CHANGELOG file"
   homepage "https://github.com/marcaddeo/cl"
-  url "https://github.com/marcaddeo/cl/releases/download/0.3.0/cl-0.3.0-macOS.tar.gz"
-  sha256 "7e1bfb30356a84faa1127b1d1fca7d3e51d55fd3377bd4eeffd549820cc10094"
+  url "https://github.com/marcaddeo/cl/releases/download/0.4.0/clparse-0.3.0-x86_64-apple-darwin.tar.gz"
+  sha256 "abdbe556f03ef4e992f48a5c7b73aa34742b0d650fdc42b0eeee8e0e32e7650a"
 
   def install
     bin.install "cl"
