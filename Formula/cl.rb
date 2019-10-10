795ac3ab7e90df9c2c54efa057445e867c5b96fc
Marc Addeo <marcaddeo@gmail.com>
2019-10-10 16:26:47 -0400

    Fix cl release link

--- Formula/cl.rb
+++ Formula/cl.rb
@@ -1,8 +1,8 @@
 class Cl < Formula
   desc "Command-line tool for managing a Keep A Changelog format CHANGELOG file"
   homepage "https://github.com/marcaddeo/cl"
-  url "https://github.com/marcaddeo/cl/releases/download/0.4.0/clparse-0.3.0-x86_64-apple-darwin.tar.gz"
-  sha256 "abdbe556f03ef4e992f48a5c7b73aa34742b0d650fdc42b0eeee8e0e32e7650a"
+  url "https://github.com/marcaddeo/cl/releases/download/0.4.0/cl-0.4.0-x86_64-apple-darwin.tar.gz"
+  sha256 "5c3b2dab9a05650e062d57f1da68fe32f14730a429bcc927c9b268a4a917d1d2"
 
   def install
     bin.install "cl"
