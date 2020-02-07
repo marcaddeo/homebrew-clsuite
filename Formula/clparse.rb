75eab5505bb0353dee3c10a917c21bef78f1f268
Marc Addeo <marcaddeo@gmail.com>
2020-02-07 12:31:57 -0500

    Fix SHA

--- Formula/clparse.rb
+++ Formula/clparse.rb
@@ -2,7 +2,7 @@ class Clparse < Formula
   desc "Command-line tool for parsing Keep A Changelog formatted CHANGELOGs"
   homepage "https://github.com/marcaddeo/clparse"
   url "https://github.com/marcaddeo/clparse/releases/download/0.5.0/clparse-0.5.0-x86_64-apple-darwin.tar.gz"
-  sha256 "b5a7443d6064d3f44a7124436246c2a04d950d6903996f536dc70dcee91bfbbb"
+  sha256 "6ebee870ea39f3df0c6c20509bf781ec78325b3c15fb7436c3adf5403e33696d"
 
   def install
     bin.install "clparse"
