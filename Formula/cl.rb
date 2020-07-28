77afb5cf22740ca9510e2c60411ffb9637f5bf0e
Marc Addeo <marcaddeo@gmail.com>
2020-07-28 15:10:49 -0400

    Bump cl to 0.9.0

--- Formula/cl.rb
+++ Formula/cl.rb
@@ -1,8 +1,8 @@
 class Cl < Formula
   desc "Command-line tool for managing a Keep A Changelog format CHANGELOG file"
   homepage "https://github.com/marcaddeo/cl"
-  url "https://github.com/marcaddeo/cl/releases/download/0.8.0/cl-0.8.0-x86_64-apple-darwin.tar.gz"
-  sha256 "7b924fed7ca221295a631664e825158b888041271d4578743baf80ea48c35e8b"
+  url "https://github.com/marcaddeo/cl/releases/download/0.9.0/cl-0.9.0-x86_64-apple-darwin.tar.gz"
+  sha256 "5ca224863e1321790234a182390119d30f9659e8d1414be8464920965ba9e2d2"
 
   def install
     bin.install "cl"
