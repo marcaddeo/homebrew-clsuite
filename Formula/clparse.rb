7f70be6ad7229626aa740d7c5c5829ec738cb63c
Marc Addeo <marcaddeo@gmail.com>
2020-10-16 11:34:42 -0400

    Bump clparse from 0.8.0 to 0.8.1

--- Formula/clparse.rb
+++ Formula/clparse.rb
@@ -1,8 +1,8 @@
 class Clparse < Formula
   desc "Command-line tool for parsing Keep A Changelog formatted CHANGELOGs"
   homepage "https://github.com/marcaddeo/clparse"
-  url "https://github.com/marcaddeo/clparse/releases/download/0.8.0/clparse-0.8.0-x86_64-apple-darwin.tar.gz"
-  sha256 "681e24ef6f6c856cc29315864092a8f40cdf39a78de6d6c7766df1d5711fd052"
+  url "https://github.com/marcaddeo/clparse/releases/download/0.8.1/clparse-0.8.1-x86_64-apple-darwin.tar.gz"
+  sha256 "4051f4bdf2f2065ac32162d88cf0f2c9b9cb09cf8c23633382225f5fd489f31e"
 
   def install
     bin.install "clparse"
