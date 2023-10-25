1933c0aecc5d3dafae84bf9b6a72a025236cc344
Marc Addeo <marcaddeo@gmail.com>
2023-10-24 21:24:56 -0400

    Bump clparse and support arm64

--- Formula/clparse.rb
+++ Formula/clparse.rb
@@ -1,8 +1,18 @@
 class Clparse < Formula
   desc "Command-line tool for parsing Keep A Changelog formatted CHANGELOGs"
   homepage "https://github.com/marcaddeo/clparse"
-  url "https://github.com/marcaddeo/clparse/releases/download/0.8.1/clparse-0.8.1-x86_64-apple-darwin.tar.gz"
-  sha256 "4051f4bdf2f2065ac32162d88cf0f2c9b9cb09cf8c23633382225f5fd489f31e"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/marcaddeo/clparse/releases/download/0.9.1/clparse-0.9.1-aarch64-apple-darwin.tar.gz"
+      sha256 "e88f0a492c65b0d8d20a5ae2207ec508b0c9d3d87015f61f95451048382d3815"
+    end
+
+    on_intel do
+      url "https://github.com/marcaddeo/clparse/releases/download/0.9.1/clparse-0.9.1-x86_64-apple-darwin.tar.gz"
+      sha256 "b4febf3f65a8c3d48b3de993637daa9803beba7f38892b116c612120c66850e8"
+    end
+  end
 
   def install
     bin.install "clparse"
