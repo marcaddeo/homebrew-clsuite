75c1922c8fc622debb39b36d6fa1ed261f34d0b3
Marc Addeo <marcaddeo@gmail.com>
2026-01-20 19:59:03 -0500

    Bump clparse from 0.9.1 to 0.9.2

--- Formula/clparse.rb
+++ Formula/clparse.rb
@@ -4,13 +4,13 @@ class Clparse < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/marcaddeo/clparse/releases/download/0.9.1/clparse-0.9.1-aarch64-apple-darwin.tar.gz"
-      sha256 "bb4d20c5d24e8221505f249aafd1219780d7d07fcc4531babf043147d5e44e83"
+      url "https://github.com/marcaddeo/clparse/releases/download/0.9.2/clparse-0.9.2-aarch64-apple-darwin.tar.gz"
+      sha256 "bd10fd1032b32525554d079a392c23ee63a8d921b2d85bbf8cd797f330842054"
     end
 
     on_intel do
-      url "https://github.com/marcaddeo/clparse/releases/download/0.9.1/clparse-0.9.1-x86_64-apple-darwin.tar.gz"
-      sha256 "e691bf83066b971362667b1847f2467f0ad175f51c11f25bcfbbf6b8c1d33844"
+      url "https://github.com/marcaddeo/clparse/releases/download/0.9.2/clparse-0.9.2-x86_64-apple-darwin.tar.gz"
+      sha256 "4f932c77d136aa8c46df6d44a7703dd624836400007b8ef5b864ddcf21135390"
     end
   end
 
